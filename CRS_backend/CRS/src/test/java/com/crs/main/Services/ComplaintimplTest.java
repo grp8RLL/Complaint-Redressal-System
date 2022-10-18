7a0303b148e36d6a68863eeda3da9b60c1e67ef2
new name <vaishnavirawat2902@gmail.com>
2022-10-18 09:45:05 +0530

    testing updated

--- CRS_backend/CRS/src/test/java/com/crs/main/Services/ComplaintimplTest.java
+++ CRS_backend/CRS/src/test/java/com/crs/main/Services/ComplaintimplTest.java
@@ -1,40 +1,36 @@
-//package com.crs.main.Services;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import java.util.List;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-//
-//import com.crs.main.model.Complaints;
-//import com.crs.main.repository.ComplaintRepository;
-//
-//@DataJpaTest
-//public class ComplaintimplTest {
-//
-//	@Autowired
-//	private ComplaintRepository ComplaintRepo;
-//
-//
-//
-//	@Autowired
-//	private TestEntityManager entityManager;
-//
-//	@Test
-//	public void returnUserFromName() {
-//		Complaints testComplaint = new Complaints();
-//		testComplaint.setTicketId(101);
-//		testComplaint.setStatus("Raised");
-//		testComplaint.setPincode("474011");
-//		testComplaint.setCustomerEmail("max12@example.com");
-//		testComplaint.setComplaint("Broadband Services Not Working Properly");
-//		entityManager.merge(testComplaint);
-//		entityManager.flush();
-//
-//		List<Complaints> found = ComplaintRepo.findComplaintByPincode(testComplaint.getPincode());
-//
-//		assertEquals(((Complaints) found).getPincode(), testComplaint.getPincode());
-//	}
-//
-//}
+package com.crs.main.Services;
+
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.crs.main.model.Complaints;
+import com.crs.main.repository.ComplaintRepository;
+
+
+@SpringBootTest
+public class ComplaintimplTest {
+
+	@Autowired
+	private ComplaintRepository ComplaintRepo;
+
+
+	@Test
+	public void returnUserFromName() {
+		Complaints testComplaint = new Complaints();
+		testComplaint.setTicketId(101);
+		testComplaint.setStatus("Raised");
+		testComplaint.setPincode("474011");
+		testComplaint.setCustomerEmail("max12@example.com");
+		testComplaint.setComplaint("Broadband Services Not Working Properly");
+		
+		ComplaintRepo.save(testComplaint);
+		assertNotNull(ComplaintRepo.findById(1));
+	}
+
+}
--- CRS_backend/CRS/src/test/java/com/crs/main/Services/FeedbackimplTest.java
+++ CRS_backend/CRS/src/test/java/com/crs/main/Services/FeedbackimplTest.java
@@ -1,5 +1,34 @@
 package com.crs.main.Services;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import com.crs.main.model.Feedbacks;
+import com.crs.main.repository.FeedbackRepository;
+
+@SpringBootTest
 public class FeedbackimplTest {
+	
+	@Autowired
+	private FeedbackRepository FeedbackRepo;
+	
+	@Test
+	public void returnUserFromName() {
+		Feedbacks testFeedback = new Feedbacks();
+		testFeedback.setTicketId(101);
+		testFeedback.setFeedback("Great Customer Services!");
+		
+		testFeedback.setCustomerEmail("max12@example.com");
+		
+		
+		FeedbackRepo.save(testFeedback);
+		assertNotNull(FeedbackRepo.findById(1));
+	}
+	
+	
 
 }
